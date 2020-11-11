cab7b2dccf9f71bb41cf015e82d5ce462cce6861
T-Troll <rk@whowashere.ru>
2020-11-11 14:08:00 +0700

    TFLite support

--- NoMoreCatZ.cpp
+++ NoMoreCatZ.cpp
@@ -1,14 +1,35 @@
 // NoMoreCatZ.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#define _TFLITE
+
 #include <iostream>
+#include <windows.h>
+#ifdef _TFLITE
+//#include "tensorflow/lite/c/c_api.h"
+//#include "tensorflow/lite/c/c_api_internal.h"
+#include "tensorflow/lite/c/c_api_experimental.h"
+/*#include "absl/memory/memory.h"
+#include "tensorflow/lite/delegates/nnapi/nnapi_delegate.h"
+#include "tensorflow/lite/delegates/xnnpack/xnnpack_delegate.h"
+//#include "tensorflow/lite/examples/label_image/bitmap_helpers.h"
+#include "tensorflow/lite/examples/label_image/get_top_n.h"
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/optional_debug_tools.h"
+#include "tensorflow/lite/profiling/profiler.h"
+#include "tensorflow/lite/string_util.h"
+#include "tensorflow/lite/tools/command_line_flags.h"
+#include "tensorflow/lite/tools/delegates/delegate_provider.h"
+#include "tensorflow/lite/tools/evaluation/utils.h"*/
+#pragma comment(lib,"tensorflow2/lib/tensorflowlite_c.dll.if.lib")
+#else
 #include "tensorflow/c/c_api.h"
-//#include <opencv/highgui.h>
-//#include <opencv/cv.h>
+#pragma comment(lib,"tensorflow2/lib/tensorflow.lib")
+#endif
+
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
-//#include <opencv2/imgproc/types_c.h>
 
 #ifdef _DEBUG
 #pragma comment(lib,"opencv/lib/opencv_world343d.lib")
@@ -21,35 +42,62 @@ using namespace cv;
 
 void NoOpDeallocator(void* data, size_t a, void* b) {}
 
-void free_buffer(void* data, size_t length) { free(data); }
-
-/*TF_Buffer* read_file(const char* file) {
-    FILE* f = fopen(file, 256, "rb");
-    fseek(f, 0, SEEK_END);
-    long fsize = ftell(f);
-    fseek(f, 0, SEEK_SET);  //same as rewind(f);                                            
-
-    void* data = malloc(fsize);
-    fread(data, fsize, 1, f);
-    fclose(f);
-
-    TF_Buffer* buf = TF_NewBuffer();
-    buf->data = data;
-    buf->length = fsize;
-    buf->data_deallocator = free_buffer;
-    return buf;
-}*/
 
 int main(int argc, char* argv[])
 {
-    cout << "NoMoreCatZ v0.0.1\n";
-    cout << "Tensorflow " << TF_Version() << endl;
+    cout << "NoMoreCatZ v0.1.0\n";
+    //cout << "Tensorflow " << TF_Version() << endl;
     if (argc < 2) {
-        cout << "Usage NoMoreCatz <path to image file>\n";
+        cout << "Usage:" << endl
+            << "NoMoreCatz -register\t\t- Register app for Chrome" << endl
+            << "NoMoreCatz <path to image file>\t- Process image and type results" << endl;
     }
     else {
         string url = argv[1];
-        cout << "URL: " << url << endl;
+        //cout << "URL: " << url << endl;
+        if (url == "-register") {
+            // add to registry
+            cout << "Registering plugin...";
+            DWORD dwDisposition;
+            HKEY hKey1 = NULL;
+            RegCreateKeyEx(HKEY_CURRENT_USER,
+                TEXT("SOFTWARE\\Google\\Chrome\\NativeMessagingHosts\\com.ttroll.nomorecatz"),
+                0,
+                NULL,
+                REG_OPTION_NON_VOLATILE,
+                KEY_ALL_ACCESS,//KEY_WRITE,
+                NULL,
+                &hKey1,
+                &dwDisposition);
+            char pathBuffer[2048];
+            GetModuleFileNameA(NULL, pathBuffer, 2047);
+            GetCurrentDirectoryA(2047, pathBuffer);
+            string pathMan(pathBuffer);
+            pathMan += "\\Plugin\\nomorecats.json";
+            RegSetValueExA(hKey1, "", 0, REG_SZ, (BYTE*)pathMan.c_str(), pathMan.size());
+            cout << " Done!" << endl;
+            return 1;
+        }
+#ifdef _TFLITE
+        cout << "Tensorflow " << TfLiteVersion() << endl;
+        //********* Read model
+        TfLiteModel* model = TfLiteModelCreateFromFile("./model/lite/model.tflite");
+        TfLiteInterpreterOptions* options = TfLiteInterpreterOptionsCreate();
+        TfLiteInterpreterOptionsSetNumThreads(options, 16);
+
+        TfLiteInterpreter* interpreter = TfLiteInterpreterCreate(model, options);
+        /*tflite::FlatBufferModel* model = NULL;// ("./linear.tflite", NULL);
+        model->BuildFromFile("./model/lite/model.tflite");
+
+        tflite::ops::builtin::BuiltinOpResolver resolver;
+        std::unique_ptr<tflite::Interpreter> interpreter;
+        tflite::InterpreterBuilder(*model, resolver)(&interpreter);
+
+        // Resize input tensors, if desired.
+        interpreter->AllocateTensors();*/
+    
+#else
+        cout << "Tensorflow " << TF_Version() << endl;
         //********* Read model
         TF_Graph* Graph = TF_NewGraph();
         TF_Status* Status = TF_NewStatus();
@@ -99,18 +147,54 @@ int main(int argc, char* argv[])
         TF_Tensor** InputValues = (TF_Tensor**)malloc(sizeof(TF_Tensor*) * NumInputs);
         TF_Tensor** OutputValues = (TF_Tensor**)malloc(sizeof(TF_Tensor*) * NumOutputs);
 
-        //IplImage* img = cvLoadImage(url.c_str(), CV_LOAD_IMAGE_COLOR);
-        //IplImage* dst = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 3);
-        //cvCvtColor(img, dst, CV_BGR2RGB);
-        Mat img = imread(url.c_str()), dst;
-        cvtColor(img, dst, CV_BGR2RGB);
-
+#endif
+        Mat img = imread(url.c_str()), dstp, dst;
+        if (img.size == 0) {
+            cout << "Incorrect image file!" << endl;
+            return 0;
+        }
+        cvtColor(img, dstp, CV_BGR2RGB);
+        float rcratio = (float)img.cols / img.rows;
+        /*if (img.cols > img.rows)
+            resize(dstp, dstp, Size(320, 320 / rcratio));
+        else
+            resize(dstp, dstp, Size(320 * rcratio, 320));
+        if (rcratio != 1.0f)
+            copyMakeBorder(dstp, dstp, 0, 320 - dstp.rows, 0, 320-dstp.cols, BORDER_CONSTANT, 0);*/
+        cv::resize(dstp, dstp, Size(320, 320));
+        dstp.convertTo(dst, CV_32FC3);// , 1, -127.5f);
+        cv::normalize(dst, dst, -1.0f, 1.0f, NORM_MINMAX);
+        int ndata = 3 * dst.cols * dst.rows *sizeof(float);
         int ndims = 4;
-        //int64_t dims[] = { 1,img->width,img->height,3 };
-        int64_t dims[] = { 1,img.rows,img.cols,3 };
-        //uint8_t data[] = { 20,20,20,20,20,20,20,20,20,20,20,20 };
-        int ndata = 3*img.cols*img.rows*sizeof(uint8_t); // This is tricky, it number of bytes not number of element
+        int dims[] = { 1,dst.rows,dst.cols,3 };
+#ifdef _TFLITE
+        //TfLiteInterpreterAllocateTensors(interpreter);
+        //TfLiteInterpreterResizeInputTensor(interpreter, 0, dims, ndims);
+        TfLiteInterpreterAllocateTensors(interpreter);
+        TfLiteTensor* input_tensor =
+            TfLiteInterpreterGetInputTensor(interpreter, 0);
+        cout << TfLiteTensorCopyFromBuffer(input_tensor, dst.ptr(), 
+            ndata) << endl;
+  
+        cout << TfLiteInterpreterInvoke(interpreter) << endl;
 
+        int oCount = TfLiteInterpreterGetOutputTensorCount(interpreter);
+
+        float* scores, *types, *boxes;
+        float numDetections;
+        
+        const TfLiteTensor* output_tensor[4];
+        for (int i = 0; i < oCount; i++) 
+            output_tensor[i] = TfLiteInterpreterGetOutputTensor(interpreter, i);
+        /*int odims = TfLiteTensorNumDims(output_tensor);
+        int odimsize = TfLiteTensorDim(output_tensor,0),
+            odimsize2 = TfLiteTensorDim(output_tensor, 1);*/       
+        numDetections = *(float*)TfLiteTensorData(output_tensor[3]);
+        scores = (float*)TfLiteTensorData(output_tensor[2]);
+        types = (float*)TfLiteTensorData(output_tensor[1]);
+        boxes = (float*)TfLiteTensorData(output_tensor[0]);
+        
+#else
         TF_Tensor* int_tensor = TF_NewTensor(TF_UINT8, dims, ndims, dst.ptr(), ndata, &NoOpDeallocator, 0);
         //TF_Tensor* int_tensor = TF_NewTensor(TF_UINT8, dims, ndims, dst->imageData, dst->imageSize, &NoOpDeallocator, 0);
         if (int_tensor == NULL) {
@@ -139,9 +223,10 @@ int main(int argc, char* argv[])
         float* types = (float*)TF_TensorData(OutputValues[2]);
         float* scores = (float*)TF_TensorData(OutputValues[4]);
 
-        cvtColor(dst, img, CV_RGB2BGR);
+#endif 
 
         printf("Detection results:\n");
+        char tid[256];
         for (int i = 0; i < numDetections; i++)
             if (scores[i] > 0.4) {
                 printf("Type %f - Score %f\n", types[i], scores[i]);
@@ -150,18 +235,31 @@ int main(int argc, char* argv[])
                 rectangle(img, Point(boxes[i * 4+1] * img.cols, boxes[i * 4] * img.rows),
                     Point(boxes[i * 4 + 3] * img.cols, boxes[i * 4 + 2] * img.rows),
                         Scalar(types[i]*2.8, 255 - types[i]*2.8, types[i] * 2.8), 2);
+                sprintf_s(tid, 255, "Type: %d", (int)types[i] + 1);
+                putText(img, tid, Point(boxes[i * 4 + 1] * img.cols, boxes[i * 4] * img.rows), FONT_HERSHEY_COMPLEX_SMALL,
+                    1, Scalar(0,0,255));
             }
-            else break;
-        
+            else break; 
+
+        //cvtColor(dst, img, CV_RGB2BGR);
         string outname = url + ".out.png";
         //cvSaveImage(outname.c_str(), dst);
         imwrite(outname.c_str(), img);
+        cvtColor(dst, dst, CV_RGB2BGR);
+        imwrite((url + ".dbg.png").c_str(), dst);
 
         // deinit
+#ifdef _TFLITE
+        // Dispose of the model and interpreter objects.
+        TfLiteInterpreterDelete(interpreter);
+        TfLiteInterpreterOptionsDelete(options);
+        TfLiteModelDelete(model);
+#else
         TF_DeleteGraph(Graph);
         TF_DeleteSession(Session, Status);
         TF_DeleteSessionOptions(SessionOpts);
         TF_DeleteStatus(Status);
+#endif
     }
     return 1;
 }
